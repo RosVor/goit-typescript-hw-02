076c48d75e6f68fb2f498e2397a199b0502f02c1
RosVor <ros.vorokh@gmail.com>
2024-02-15 19:26:33 +0200

    2 and 3 fix

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,5 +1,19 @@
-let anything: any = -20;
-anything = 'Text';
-anything = {};
-
-export {};
+type AllType = {
+    name: string;
+    position: number;
+    color: string;
+    weight: number;
+  }
+  
+  function compare<
+    T extends Pick<AllType, "name" | "color">,
+    U extends Pick<AllType, "position" | "weight">
+  >(top: T, bottom: U): AllType {
+    return {
+      name: top.name,
+      color: top.color,
+      position: bottom.position,
+      weight: bottom.weight,
+    };
+  }
+  
--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -3,4 +3,7 @@ some = 'Text';
 
 let str: string = some as string;
 
+const obj: object = {};
+Object.assign(obj, some);
+
 export {};
